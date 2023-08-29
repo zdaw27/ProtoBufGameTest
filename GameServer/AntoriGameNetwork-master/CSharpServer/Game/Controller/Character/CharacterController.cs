8a7636d355a98d06a5f6b708c796ce312a2ec0ec
hwi kim <51247612+zdaw27@users.noreply.github.com>
2023-08-30 04:59:59 +0900

    델타타임 추가.

--- GameServer/AntoriGameNetwork-master/CSharpServer/Game/Controller/Character/CharacterController.cs
+++ GameServer/AntoriGameNetwork-master/CSharpServer/Game/Controller/Character/CharacterController.cs
@@ -43,7 +43,7 @@ abstract class CharacterController : TickBase {
                 //    pc.SetPos(new Vector2(pc.pos.X, pc.pos.Y - (elapsedTime / 1000) * pc.stat.SPEED));
                 //}
 
-                pc.SetPos(pc.pos + pc.stat.SPEED * pc.dir * 0.2f);
+                pc.SetPos(pc.pos + pc.stat.SPEED * pc.dir * Program.deltaTime);
 
                 BroadCast_RefreshPos(pc);
             }
--- GameServer/AntoriGameNetwork-master/CSharpServer/Program.cs
+++ GameServer/AntoriGameNetwork-master/CSharpServer/Program.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 
 class Program {
 
+    public static float deltaTime = 0f;
+
     static void Main(string[] args) {
         ProtocolManager.GetInstance().Register();
         ProtocolDispatcher.GetInstance().Register();
@@ -12,19 +14,23 @@ class Program {
         BattleServer.GetInstance().Start();
 
         var sw = new Stopwatch();
+        
         int frameLimit = 1000 / 30; // 1000ms / 30fps
         while (true) {
             sw.Restart();
-
+            
             foreach (var zoneController in BattleServer.GetInstance().GetZoneControllerPool()) {
                 Task.Run(() => {
                     zoneController.Update();
                 });
             }
+            
 
             sw.Stop();
             var elapsed = (int)sw.ElapsedMilliseconds;
+            
             var sleepDuration = frameLimit - elapsed;
+            deltaTime = (float)sleepDuration * 0.001f;
             if (sleepDuration > 0) {
                 Thread.Sleep(sleepDuration);
             }
